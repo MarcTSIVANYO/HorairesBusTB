dc490feb4b40ef7f4bf9ec664fe39db5bded2382
Tsivanyo Kodjo Nuwoza <kodjo-nuwoza.tsivanyo@etudiant.univ-rennes1.fr>
2020-12-03 17:10:58 +0100

    init commit

---
+++ app/src/main/java/fr/istic/mob/star2tb/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.istic.mob.star2tb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
